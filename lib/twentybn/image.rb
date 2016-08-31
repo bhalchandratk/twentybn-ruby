85f5ef43d66c87013b23ea391fcbcc8b8d0383b2
Valentin Haenel <valentin@haenel.co>
2016-08-31 14:42:32 +0200

    adding the model parameter to the tag_image endpoint

--- lib/twentybn/image.rb
+++ lib/twentybn/image.rb
@@ -32,14 +32,15 @@ module TwentyBN
       return response.body
     end
 
-    def tag
+    def tag(model=:objects)
       response = api_connection.post do |request|
         request.headers['Content-Type'] = 'application/json'
         request.headers['X-ApiKey'] = TwentyBN.config[:api_key]
         data = {
           content: self.image_base64,
         }
-        request.url '/v1/tag_image'
+        request.url "/v1/tag_image"
+        request.params['model'] = model
         request.body = JSON.generate(data)
       end
       return response.body
